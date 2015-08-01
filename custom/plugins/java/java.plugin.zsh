d8c113e76b819cf20f965d519fee03ec28814dd4
Tommy Becker <twbecker@gmail.com>
2015-07-31 21:33:47 -0400

    Java plugin fixes.
    Move JAVA_HOME definition out of plugin, enable use of /usr/libexec/java_home on OS X.

--- custom/plugins/java/java.plugin.zsh
+++ custom/plugins/java/java.plugin.zsh
@@ -1,19 +1,25 @@
-export JAVA_HOME=/opt/jdk1.8
 alias jps="jps -l"
 if whence mvn-color > /dev/null; then
     alias mvn=mvn-color
 fi
 
 function switchJdk() {
-    if [ ! -d $1 ]; then
-        print "$1 is not a valid JAVA_HOME"
+    # If we're on a mac, allow passing just the version (e.g. 1.7)
+    if [[ $(uname) == "Darwin" && ! -d $1 ]]; then
+        newJavaHome=$(/usr/libexec/java_home -v $1)
+    else 
+        # Otherwise we need the directory
+        newJavaHome=$1
+    fi
+    if [[ ! -d $newJavaHome ]]; then
+        print "$newJavaHome is not a valid JAVA_HOME"
         return 1
     fi
     let index=${path[(I)$JAVA_HOME/bin]}
     if (($index)); then
-        path[$index]=$1/bin
+        path[$index]=$newJavaHome/bin
     else 
-        path=($1/bin $path)
+        path=($newJavaHome/bin $path)
     fi
-    export JAVA_HOME=$1
+    export JAVA_HOME=$newJavaHome
 }
