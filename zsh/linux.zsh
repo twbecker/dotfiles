0ab3e8dce6c32f003f1cf994689f3c42a1e6975d
Tommy Becker <twbecker@gmail.com>
2018-01-03 16:20:22 -0500

    Add coloring to ls and completion on Linux.

--- zsh/linux.zsh
+++ zsh/linux.zsh
@@ -1,2 +1,7 @@
+eval $(dircolors)
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+
+alias ls='ls --color=auto' # -G on OSX :/
+
 source $DOTFILES/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source $DOTFILES/zsh-history-substring-search/zsh-history-substring-search.zsh
