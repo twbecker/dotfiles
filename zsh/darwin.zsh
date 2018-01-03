e26661ad61c138ef43971bac2380055df0389734
Tommy Becker <twbecker@gmail.com>
2018-01-02 20:48:16 -0500

    Add files for OS specific commands.

---
+++ zsh/darwin.zsh
@@ -0,0 +1,6 @@
+# Brew installed locations
+source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source /usr/local/share/zsh-history-substring-search/zsh-history-substring-search.zsh
+[[ -e "${HOME}/.iterm2_shell_integration.zsh" ]] && source "${HOME}/.iterm2_shell_integration.zsh"
+[[ -e /usr/local/opt/chruby/share/chruby/chruby.sh ]] && source /usr/local/opt/chruby/share/chruby/chruby.sh
+[[ -e /usr/local/opt/chruby/share/chruby/auto.sh ]] && source /usr/local/opt/chruby/share/chruby/auto.sh
---
+++ zsh/linux.zsh
@@ -0,0 +1,2 @@
+source $DOTFILES/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source $DOTFILES/zsh-history-substring-search/zsh-history-substring-search.zsh
