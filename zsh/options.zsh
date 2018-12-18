617ed532e2bae7da1607a2cb33a049b07fa74f01
Tommy Becker <twbecker@gmail.com>
2018-12-17 19:43:24 -0500

    Allow for placing completion functions (without autoloading them), in zsh/functions.

--- zsh/options.zsh
+++ zsh/options.zsh
@@ -1,15 +1,11 @@
 setopt auto_cd
 setopt auto_pushd
-setopt pushd_ignore_dups
-
+setopt no_auto_remove_slash
+setopt cdable_vars
+setopt correct
+setopt extended_glob
 setopt extended_history
 setopt hist_ignore_all_dups
 setopt hist_verify
-
 setopt interactive_comments
-
-setopt extended_glob
-
-set -o no_auto_remove_slash
-# https://github.com/robbyrussell/oh-my-zsh/commit/ed484dfaf63c6d8789b2bd2e952a07bbbb9ad330
-set -o cdable_vars
+setopt pushd_ignore_dups
