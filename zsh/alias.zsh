4a03d964bfc03f7410dc86085cb65ba1d64d49c5
Tommy Becker <thomas.becker@xperi.com>
2022-12-31 12:46:52 -0500

    Add gw alias.

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -6,6 +6,7 @@ alias ......=../../../../..
 alias d='dirs -v'
 alias import='fc -RI'
 alias grep='grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn,.terraform}'
+alias gw=gradlew
 alias ll='ls -l'
 alias tf='tail -F'
 alias p='cd ~/projects'
