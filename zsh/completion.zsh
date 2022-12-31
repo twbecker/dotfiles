373157846514b21ee04ceb516591675230da2429
Tommy Becker <twbecker@gmail.com>
2022-12-31 12:36:25 -0500

    Add completion for op.

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -25,3 +25,7 @@ zstyle ':completion::complete:*' use-cache 1
 
 # Show full command line on process completion
 zstyle ':completion:*:*:*:*:processes' command 'ps -U $USER -o pid,user,cmd'
+
+if (( $+commands[op] )); then
+  eval "$(op completion zsh)"; compdef _op op
+fi
