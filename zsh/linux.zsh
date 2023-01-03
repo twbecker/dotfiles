608d08a67d347461090ce79c71d7aa665101aa33
Tommy Becker <twbecker@gmail.com>
2023-01-03 08:05:39 -0500

    Allow line wrapping for systemd.

--- zsh/linux.zsh
+++ zsh/linux.zsh
@@ -1,6 +1,7 @@
 eval $(dircolors)
 zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 
+export SYSTEMD_LESS=FRXMK # Remove S to allow line wrapping
 alias ls='ls --color=auto' # -G on OSX :/
 
 source $DOTFILES/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
