576578f02ce938a4b46ab87776058aadb7294aa7
Tommy Becker <thomas.becker@xperi.com>
2023-02-20 16:34:58 -0500

    Source 1password plugin aliases.

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -8,5 +8,7 @@ alias import='fc -RI'
 alias grep='grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn,.terraform}'
 alias gw=gradlew
 alias ll='ls -l'
+alias ls='ls --color=auto -F'
 alias tf='tail -F'
 alias p='cd ~/projects'
+[[ -f /Users/tobecker/.config/op/plugins.sh ]] && source /Users/tobecker/.config/op/plugins.sh
