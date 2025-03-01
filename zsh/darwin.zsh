f1a03ce122f8b8c79b82de3856a35b8675de8c5c
Tommy Becker <thomas.becker@xperi.com>
2025-03-01 16:57:47 -0500

    Remove support for homebrew on Intel.

--- zsh/darwin.zsh
+++ zsh/darwin.zsh
@@ -1,13 +1,6 @@
-# Brew installed locations
-if [[ $(uname -m) = "arm64" ]]; then
-    BREW_HOME=/opt/homebrew
-else
-    BREW_HOME=/usr/local
-fi
-eval $($BREW_HOME/bin/brew shellenv)
-fpath=($BREW_HOME/share/zsh/site-functions $fpath)
-source $BREW_HOME/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-source $BREW_HOME/share/zsh-history-substring-search/zsh-history-substring-search.zsh
+eval $(/opt/homebrew/bin/brew shellenv)
+source $HOMEBREW_PREFIX/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source $HOMEBREW_PREFIX/share/zsh-history-substring-search/zsh-history-substring-search.zsh
 [[ -e "${HOME}/.iterm2_shell_integration.zsh" ]] && source "${HOME}/.iterm2_shell_integration.zsh"
 
 zstyle ':completion:*' list-colors 'di=34:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
