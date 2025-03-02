6ace3ea0376b3c5b052840bc6d69f7be4bc621b6
Tommy Becker <thomas.becker@xperi.com>
2025-03-02 12:26:59 -0500

    Update aliases.

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -6,9 +6,13 @@ alias ......=../../../../..
 alias d='dirs -v'
 alias import='fc -RI'
 alias grep='grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn,.terraform}'
+alias gb='gradlew build'
 alias gw=gradlew
+alias j=just
 alias ll='ls -l'
 alias ls='ls --color=auto -F'
+alias mvnci='mvn clean install'
+alias mvni='mvn install'
 alias tf='tail -F'
 alias p='cd ~/projects'
 [[ -f /Users/tobecker/.config/op/plugins.sh ]] && source /Users/tobecker/.config/op/plugins.sh
