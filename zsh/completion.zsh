cf1e9db63ce953417e71515d64915d780300b528
Tommy Becker <thomas.becker@xperi.com>
2025-03-02 12:28:34 -0500

    Fix op completion.

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -27,3 +27,4 @@ zstyle ':completion::complete:*' use-cache 1
 zstyle ':completion:*:*:*:*:processes' command 'ps -U $USER -o pid,user,cmd'
 
 autoload -U +X bashcompinit && bashcompinit
+(( $+commands[op] )) && eval "$(op completion zsh)" && compdef _op op
