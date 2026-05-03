11d86c1b6839be7348955dca7713fc52446d2cfd
Tommy Becker <thomas.becker@xperi.com>
2026-05-03 12:11:48 -0400

    Switch to zsh-patina for highlighting

--- zsh/darwin.zsh
+++ zsh/darwin.zsh
@@ -1,6 +1,6 @@
 eval $(/opt/homebrew/bin/brew shellenv)
-source $HOMEBREW_PREFIX/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+eval "$(zsh-patina activate)"
 source $HOMEBREW_PREFIX/share/zsh-history-substring-search/zsh-history-substring-search.zsh
-[[ -e "${HOME}/.iterm2_shell_integration.zsh" ]] && source "${HOME}/.iterm2_shell_integration.zsh"
 
 zstyle ':completion:*' list-colors 'di=34:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
+export PATH=$PATH:~/Library/Application\ Support/JetBrains/Toolbox/scripts
